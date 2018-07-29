16c8893dbdb42d52ace9e4ae071fb8b5818f68de
SamLiYin <sam_ly@foxmail.com>
2018-07-29 09:41:20 -0700

    chapter_1 exercise 1.11

---
+++ chapter_1/1.11.c
@@ -0,0 +1,2 @@
+1. put one string include ' ', '\t', '\n' and '\b'
+2. '\b', because '\b' will delete the previous character in input string, but the deleted character has been put to output buffer.
